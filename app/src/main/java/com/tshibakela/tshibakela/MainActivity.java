a4a68a2873518752c2a8050293c0a012db3353d7
Tshibakela Mukovhe <tshibakelamukovhe@gmail.com>
2017-11-30 09:19:28 +0200

    first commit

---
+++ app/src/main/java/com/tshibakela/tshibakela/MainActivity.java
@@ -0,0 +1,28 @@
+package com.tshibakela.tshibakela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    public implement getView() {
+        return view;
+    }[]
+    implement view.onCliklistener
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate")
+    }
+
+}
+
+@Override
+public boolean OnCreateOptionMenu (Menu menu) {
+    // inflate the menu; this add items to the action bar if it is present.
+    getMenuInflater ().inflate (R.menu.menu_main,menu);
+    return
+
+}
